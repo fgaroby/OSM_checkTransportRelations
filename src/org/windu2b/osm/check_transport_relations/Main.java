8475d67381a27bef50f919746a8d6980ee1e3551
windu2b <windu.2b@gmail.com>
2012-11-04 10:41:00 +0100

    Main class

---
+++ src/org/windu2b/osm/check_transport_relations/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.windu2b.osm.check_transport_relations;
+
+/**
+ * @author windu
+ * 
+ */
+public class Main
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main( String[] args )
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
